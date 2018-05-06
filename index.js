aa00d90cd48015999c6413fab59493ed765422a1
thunguy@me.com <thunguy@me.com>
2018-05-06 15:05:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Thu"
-var height = "54"
+var height = 54
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 10)
+console.log(height + 10)
